664ff808c52716dfb8ed85c088d1f2e35a3c7e99
Talgat <0_talgat_0@mail.ru>
2024-02-29 18:24:32 +0500

    Добавление слайдера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,37 @@
 /* в этот файл добавляет скрипты*/
+const slider = document.querySelector('.hero__slider-wrapper');
+const prevButton = document.querySelector('.hero__arrow--left');
+const nextButton = document.querySelector('.hero__arrow--right');
+const slides = Array.from(slider.querySelectorAll('.hero__slider'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
